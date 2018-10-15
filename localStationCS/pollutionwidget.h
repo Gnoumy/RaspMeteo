433a962f4f342a76a0cf273d04aeb6c8c62858eb
gnoumy <gaucher.ju@gmail.com>
2018-10-15 14:17:23 +0200

    PollutionWidget: ajout Latitude et longitude

--- localStationCS/pollutionwidget.h
+++ localStationCS/pollutionwidget.h
@@ -24,8 +24,8 @@ public:
 private:
     Ui::PollutionWidget *ui;
 private slots:
-    void reponseUrl(QNetworkReply *data);
-    void affichageGraphique(QNetworkReply *data);
+    void premierePage(QNetworkReply *data);
+    void deuxiemePage(QNetworkReply *data);
 };
 
 #endif // POLLUTIONWIDGET_H
